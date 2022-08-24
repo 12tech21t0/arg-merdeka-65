356f73fcf82e6ccbdb7c8712eac7cd6b1f733117
mhanifmustakim <mhanifmustakim@gmail.com>
2022-08-24 19:59:42 +0800

    Complete list of quotes

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 const list = [
-    '"Jadilah cahaya, walau tak tersentuh, selalu menerangi. Jadilah angin, walau tak tampak, selalu memberi kesejukan. Jadilah sahabat sejati, walau tak bersama menjalani hari, selalu menjaga hati."',
-    '"Kebijaksanaan laksana cahaya, ia akan mencerahkan daun-daun pemikiranmu, memeluk lembut ranting pemikiranmu, dan mengukuhkan akar karaktermu."',
-    '"Tak ada yang muncul setelah kegelapan selain cahaya, tak ada yang muncul setelah kesedihan selain kebahagiaan."',
+    '"Kecantikan bukan hanya di wajah, kecantikan adalah cahaya di dalam hati."',
+    '"Yakinilah, terangnya hati lebih terang dari cahaya matahari"',
+    '"Sebelum kamu bisa melihat cahaya, kamu harus melewati kegelapan."',
+    '"Jangan takut akan bayangan karena bayangan berarti ada suatu cahaya yang bersinar di dekatnya."',
+    '"Meski matahari menolak untuk bersinar, aku akan tetap di sini dan menjadi cahaya untukmu."',
+    'Kegelapan bukan ancaman, hanya peluang untuk menghidupkan cahaya."',
+    '"Jadilah cahaya yang selalu menerangi sekitarnya di manapun melangkah."',
 ]
 
 function getRandomQuote() {
